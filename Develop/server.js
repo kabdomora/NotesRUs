83103ec922602f43e17f15866bf1300004d38c1d
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-13 17:07:33 -0800

    added blank routes files and added app.use context for server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const notes = require('./db/db.json');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static(path.join(__dirname, '/api/notes')));
+// for use in get/post routes.
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
