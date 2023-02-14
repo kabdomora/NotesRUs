3aac23d6dfa2c7b7f130ff77892e40f39bde6d09
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-13 17:40:16 -0800

    commented server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -8,11 +7,22 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-// for use in get/post routes.
+// middleware 
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html')));
+// get homepage
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html')));
+// get notes page
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
