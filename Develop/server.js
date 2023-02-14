0eb35fa3aa42fb427c3b0e60956cd903e01a3d02
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-13 19:49:13 -0800

    corrected route to notes page

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./routes/notes');
+const html = require('./routes/html');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/api', notes);
+app.use('/', html);
 // middleware 
 
 app.use(express.static('public'));
